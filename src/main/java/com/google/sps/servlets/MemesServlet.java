4fc4ba00d925638458dbcb1932fae7d6b35b18a3
Jesus Urquidez <jesus123076@gmail.com>
2021-03-29 19:50:53 +0000

    Upload and See All Servlets

--- src/main/java/com/google/sps/servlets/MemesServlet.java
+++ src/main/java/com/google/sps/servlets/MemesServlet.java
@@ -26,12 +26,46 @@ public class MemesServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    Part filePart = request.getPart("image");
+    String fileName = UUID.randomUUID().toString();
+    InputStream fileInputStream = filePart.getInputStream();
 
+    String uploadedFileUrl = uploadToCloudStorage(fileName, fileInputStream);
+
+    PrintWriter out = response.getWriter();
+    out.println("<p>Your Meme:</p>");
+    out.println("<a href=\"" + uploadedFileUrl + "\">");
+    out.println("<img src=\"" + uploadedFileUrl + "\" />");
+    out.println("</a><br>");
   }
+
+  String uploadToCloudStorage(String fileName, InputStream fileInputStream) {
+    final String PROJECT_ID = "spring21-sps-43";
+    final String BUCKET_NAME = "spring21-sps-43.appspot.com";
+    Storage storage = StorageOptions.newBuilder().setProjectId(PROJECT_ID).build().getService();
+    BlobId blobId = BlobId.of(BUCKET_NAME, fileName);
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+
+    Blob blob = storage.create(blobInfo, fileInputStream);
+
+    return blob.getMediaLink();
+  }
+
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
 
+    final String PROJECT_ID = "spring21-sps-43";
+    final String BUCKET_NAME = "spring21-sps-43.appspot.com";
+    Storage storage = StorageOptions.newBuilder().setProjectId(PROJECT_ID).build().getService();
+    Bucket bucket = storage.get(BUCKET_NAME);
+    Page<Blob> blobs = bucket.list();
+
+    // Output <img> elements as HTML.
+    for (Blob blob : blobs.iterateAll()) {
+      String imgTag = String.format("<img src=\"%s\" width = \"300\"/>", blob.getMediaLink());
+      response.getWriter().println(imgTag);
+    }
   }
 }
-
