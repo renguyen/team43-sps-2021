ef70cb21e5993e0e4d321ba8e0978c0595785e6b
Ximena <xphere24@gmail.com>
2021-03-29 05:39:18 +0000

    Post and Get Comments function enabled. Comments Server mvp.

---
+++ src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -0,0 +1,80 @@
+package main.java.com.google.sps.servlets;
+
+import com.google.api.gax.paging.Page;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
+
+import java.util.*;
+
+@WebServlet("/comment")
+@MultipartConfig
+public class CommentsServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Show Comments.</h1>");
+    response.getWriter().println("<ul>"); // <ul> --> 'unordered list element'
+    
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+    // Setting an order by time posted
+    Query<Entity> query = Query.newEntityQueryBuilder()
+      .setKind("Comment")
+      .setOrderBy(OrderBy.desc("timestamp"))
+      .build();
+    QueryResults<Entity> results = datastore.run(query);
+
+    // Basicallly stored like(?) a list. This is printing comments.
+    while (results.hasNext()) {
+      Entity entity = results.next();
+      String message = entity.getString("text");
+      response.getWriter().println("<li>" + message + "</li>");
+    }
+    
+    response.getWriter().println("</ul>");
+    response.getWriter().println("<p><a href=\"/\">Back</a></p>"); // tbh a bit confused about this.
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Getting comment input
+    String comment = request.getParameter("comment");
+
+    // Storing ordered by time posted
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Comment");
+    FullEntity commentEntity = Entity.newBuilder(keyFactory.newKey())
+      .set("comment", comment)
+      .set("timestamp", System.currentTimeMillis())
+      .build();
+    datastore.put(commentEntity); // Adding comment to Data Store
+ 
+    // Redirect to /comment.
+    // The request will be routed to the doGet() function.
+    response.sendRedirect("/comment");
+
+  }
+
+}
